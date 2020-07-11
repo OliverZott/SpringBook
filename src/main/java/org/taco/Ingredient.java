258221fdc0d549c63c3477420454b0fa0d2b4a86
OliverZott <zott_oliver@web.de>
2020-07-11 17:23:58 +0200

    Added ingredient DTO

---
+++ src/main/java/org/taco/Ingredient.java
@@ -0,0 +1,30 @@
+package org.taco;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data		// implements missing methods, eg constructor (takes all final properties)
+@RequiredArgsConstructor		// not necessary (due to @Data) but no harm to inlcude as well
+public class Ingredient {
+	
+	private final String id;
+	private final String name;
+	private final Type type;
+	
+	public Ingredient(String id, String name, Type type) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.type = type;
+	}
+
+	public static enum Type {
+		WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString() + String.format(" (OZ) Id: %s; Name: %s", this.id, getClass());
+	}
+}
+
