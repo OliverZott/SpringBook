ac63ded1cb65edbe8006010e02f94ed245195e86
OliverZott <zott_oliver@web.de>
2020-07-17 23:59:45 +0200

    taco dto / model dummy class

--- src/main/java/org/taco/Taco.java
+++ src/main/java/org/taco/Taco.java
@@ -1,5 +1,16 @@
 package org.taco;
 
 public class Taco {
+	private String namee;
+
+	public void setName(String name) {
+		this.namee = name;
+	}
+
+	public  String getName() {
+		return namee;
+	}
+	
+	
 
 }
