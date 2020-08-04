062bc52b639b2a11416dc482d564cdefec89c655
OliverZott <zott_oliver@web.de>
2020-08-04 20:03:31 +0200

    Minor adaptation to use object instances with name different to class name

--- src/main/java/org/taco/controller/OrderController.java
+++ src/main/java/org/taco/controller/OrderController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.taco.model.Order;
@@ -30,7 +31,7 @@ public class OrderController {
 	
 
 	@PostMapping 		//("/orders")
-	public String processOrder(@Valid Order order, Errors errors) {
+	public String processOrder(@Valid @ModelAttribute("order") Order order, Errors errors) {
 		if (errors.hasErrors()) {
 			return "orderForm";
 		}
