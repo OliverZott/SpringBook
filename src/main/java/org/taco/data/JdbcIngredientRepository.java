d7faf535eb208172c67d0ae58335f8ee4ccb84da
OliverZott <zott_oliver@web.de>
2020-08-07 14:56:04 +0200

    Added Save-method and DI for jdbctemplate

--- src/main/java/org/taco/data/JdbcIngredientRepository.java
+++ src/main/java/org/taco/data/JdbcIngredientRepository.java
@@ -5,15 +5,29 @@ import org.taco.model.Ingredient;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 //import org.springframework.jdbc.core.RowMapper;
 
+
+/**
+ * Repository for Ingredient-Model
+ * 
+ * @author Oliver Zott
+ *
+ */
 @Component
 public class JdbcIngredientRepository implements IngredientRepository {
 
 	private  JdbcTemplate jdbc;
+	
+	@Autowired
+	public JdbcIngredientRepository(JdbcTemplate jdbc) {
+		this.jdbc = jdbc;
+	}
 
+	
 	@Override
 	public Iterable<Ingredient> findAll() {
 		return jdbc.query("select id, name, type from Ingredient",
@@ -38,8 +52,13 @@ public class JdbcIngredientRepository implements IngredientRepository {
 	
 	@Override
 	public Ingredient save(Ingredient ingredient) {
-		// TODO Auto-generated method stub
-		return null;
+		jdbc.update(
+				"INSERT INTO Ingredient (id, name, type) values (?, ?, ?)",
+				ingredient.getId(),
+				ingredient.getName(),
+				ingredient.getType().toString()
+				);
+		return ingredient;
 	}
-
+		
 }
