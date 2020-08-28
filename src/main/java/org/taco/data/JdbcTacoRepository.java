df49a6368d7e44b942c19f7214346656fd87a953
OliverZott <zott_oliver@web.de>
2020-08-28 15:35:49 +0200

    Saving Taco in taco-table and taco_ingredients linking table

--- src/main/java/org/taco/data/JdbcTacoRepository.java
+++ src/main/java/org/taco/data/JdbcTacoRepository.java
@@ -1,7 +1,16 @@
 package org.taco.data;
 
+import java.sql.Timestamp;
+import java.sql.Types;
+import java.util.Arrays;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.PreparedStatementCreatorFactory;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import org.taco.model.Ingredient;
@@ -13,32 +22,51 @@ public class JdbcTacoRepository implements TacoRepository {
 	private JdbcTemplate jdbc;
 	
 	@Autowired
-	public void JdbcTacoRepository(JdbcTemplate jdbc) {
+	public JdbcTacoRepository(JdbcTemplate jdbc) {
 		this.jdbc = jdbc;
 		
 	}
 
 	@Override
 	public Taco save(Taco taco) {
-		
-		long tacoId = saveTacoInfo(taco);	// ...
-		taco.setId(tacoId);		// set Id to taco-object
+		long tacoId = saveTacoInfo(taco);	// returns tacoId
+		taco.setId(tacoId);					// set Id to taco-object
 		
 		for (Ingredient ingredient : taco.getIngredients()) {
 			saveIngredientToTaco(ingredient, tacoId);
 		}
-		
 		return taco;
 	}
 
+	/*
+	 * Function to assign ingredients to linking-table (taco_ingredients)
+	 */
 	private void saveIngredientToTaco(Ingredient ingredient, long tacoId) {
-		// TODO Auto-generated method stub
-		
+		jdbc.update(
+				"INSERT INTO Taco_Ingredients (taco, ingredient) VALUES (?, ?)",
+				tacoId,
+				ingredient.getId());
 	}
 
+	/*
+	 * Function sets taco creation date/time and gives/retrieves tacoId from db!
+	 */
 	private long saveTacoInfo(Taco taco) {
-		// TODO Auto-generated method stub
-		return 0;
+		taco.setCreatedAt(new Date());				// set taco creation time/date									
+		PreparedStatementCreator psc = new PreparedStatementCreatorFactory(
+				"INSERT INTO Taco (name, createdAt) VALUES (?, ?)",
+				Types.VARCHAR, Types.TIMESTAMP
+				).newPreparedStatementCreator(
+						Arrays.asList(
+								taco.getTacoName(),
+								new Timestamp(taco.getCreatedAt().getTime())		// get timestamp from creation date
+								)
+						);
+		
+		KeyHolder keyHolder = new GeneratedKeyHolder();
+		jdbc.update(psc, keyHolder);
+		
+		return keyHolder.getKey().longValue();
 	}
 
 }
