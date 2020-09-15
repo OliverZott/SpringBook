83934a16d4df99b6f5559b6e2ad8d12014bd9285
OliverZott <zott_oliver@web.de>
2020-09-15 20:39:02 +0200

    Comenting added

--- src/main/java/org/taco/controller/WebConfig.java
+++ src/main/java/org/taco/controller/WebConfig.java
@@ -4,6 +4,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * Special simple Controller type: View-Controller
+ * (page 55)
+ *
+ * Useful to extend Configuration-classes for each type configuration (Web, Data, Security,...)
+ */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
