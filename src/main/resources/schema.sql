af1a096e7bad4afe17cbad703284f3b4bac6b675
OliverZott <zott_oliver@web.de>
2020-09-17 17:56:20 +0200

    Fixed error with wrong table properties

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -22,15 +22,15 @@ alter table Taco_Ingredients
 	
 create table if not exists Taco_Order (
 	id identity,
-	deliveryName varchar(50) not null,
-	deliveryStreet varchar(50) not null,
-	deliveryCity varchar(50) not null,
-	deliveryState varchar(2) not null,
-	deliveryZip varchar(10) not null,
+	Name varchar(50) not null,
+	Street varchar(50) not null,
+	City varchar(50) not null,
+	State varchar(20) not null,
+	Zip varchar(10) not null,
 	ccNumber varchar(16) not null,
 	ccExpiration varchar(5) not null,
 	ccCVV varchar(3) not null,
-	placedAr timestamp not null
+	placedAt timestamp not null
 );
 
 create table if not exists Taco_Order_Tacos (
