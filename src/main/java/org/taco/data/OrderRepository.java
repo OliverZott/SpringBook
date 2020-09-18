ef580a41bfc85674d8eb0102324f4a05f11ac5dd
OliverZott <zott_oliver@web.de>
2020-09-18 10:52:54 +0200

    Fooling around with Repository-Method for query

--- src/main/java/org/taco/data/OrderRepository.java
+++ src/main/java/org/taco/data/OrderRepository.java
@@ -3,8 +3,12 @@ package org.taco.data;
 import org.springframework.data.repository.CrudRepository;
 import org.taco.model.Order;
 
+import java.util.List;
+
 public interface OrderRepository extends CrudRepository<Order, Long>{
-	
+
+    List<Order> findByZip(String Zip);
+
 /*
 	Order save(Order order);
 */
